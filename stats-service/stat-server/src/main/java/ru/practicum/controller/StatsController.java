c38470c2d7fa4e375850cada86a88c5da521d5d7
Zaven <kalanderyan99@yandex.ru>
2023-08-24 00:11:15 +0300

    style:

--- stats-service/stat-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats-service/stat-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -2,14 +2,12 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.service.HitService;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 import java.util.List;
 
 
--- stats-service/stat-server/src/main/java/ru/practicum/service/HitService.java
+++ stats-service/stat-server/src/main/java/ru/practicum/service/HitService.java
@@ -3,7 +3,6 @@ package ru.practicum.service;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface HitService {
