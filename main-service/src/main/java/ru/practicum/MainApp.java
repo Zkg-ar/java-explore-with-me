022e9be1f1c91d8d173e7f1f97659e398367b8af
Zaven <kalanderyan99@yandex.ru>
2023-08-23 17:07:35 +0300

    fix

--- main-service/src/main/java/ru/practicum/MainApp.java
+++ main-service/src/main/java/ru/practicum/MainApp.java
@@ -3,7 +3,6 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
 
 @SpringBootApplication
 public class MainApp {
