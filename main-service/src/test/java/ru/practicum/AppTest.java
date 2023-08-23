41a51dfd3c3c8b46bcf12a8d831a99dd5000963e
Zaven <kalanderyan99@yandex.ru>
2023-08-23 17:11:13 +0300

    fix

--- main-service/src/test/java/ru/practicum/AppTest.java
+++ main-service/src/test/java/ru/practicum/AppTest.java
@@ -1,8 +1,9 @@
 package ru.practicum;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
