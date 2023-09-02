8dec47ba4b9a7c1d8e5660d267781ad76d826702
Zaven <kalanderyan99@yandex.ru>
2023-09-02 15:39:01 +0300

    fix

--- main-service/src/main/java/ru/practicum/Constant.java
+++ main-service/src/main/java/ru/practicum/Constant.java
@@ -1,7 +1,10 @@
 package ru.practicum;
 
+import lombok.experimental.UtilityClass;
+
 import java.time.format.DateTimeFormatter;
 
+@UtilityClass
 public class Constant {
     public final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public final static String FORMAT = "yyyy-MM-dd HH:mm:ss";
