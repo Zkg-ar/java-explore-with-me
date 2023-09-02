20be4939ccda032dcde536131128359505ca9e28
Zaven <kalanderyan99@yandex.ru>
2023-09-02 15:43:51 +0300

    fix

--- main-service/src/main/java/ru/practicum/Constant.java
+++ main-service/src/main/java/ru/practicum/Constant.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 @UtilityClass
 public class Constant {
-    public final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    public final static String FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static final String FORMAT = "yyyy-MM-dd HH:mm:ss";
 
 }
