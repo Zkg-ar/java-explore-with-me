e300effd3f923837f0bfe8154c5f2639b638dcb9
Zaven <kalanderyan99@yandex.ru>
2023-09-04 18:39:19 +0300

    style

--- main-service/src/main/java/ru/practicum/requests/service/ParticipationRequestServiceImpl.java
+++ main-service/src/main/java/ru/practicum/requests/service/ParticipationRequestServiceImpl.java
@@ -23,7 +23,6 @@ import ru.practicum.user.repository.UserRepository;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
